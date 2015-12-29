aa70e99fb2f2cdb4a6e18942db52b7c617d5f9c8
mracu <racu.mihail@gmail.com>
2015-12-29 10:46:02 +0200

    Added google map element

---
+++ orar.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+<title>Orar</title>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css\style.css">
+</head>
+<body>
+	<!-- Header -->
+	<div id="header-wrap">
+		<header>
+			<hgroup>
+				<h1>
+					<a href="index.php">arintur-trans</a>
+				</h1>
+			</hgroup>
+
+
+			<!-- Menu -->
+			<nav>
+				<ul>
+					<li><a href="index.php">Acasa</a></li>
+					<li><a id="current" href="orar.php">Orarul</a></li>
+					<li><a href="gallery.php">Galerie</a></li>
+					<li><a href="contacts.php">Contacte</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+	<!-- End Header Wrap-->
+	<div class="content-wrap">
+		<div class="titled-container">
+			<p>
+				Orarul de circulație pe ruta internațională<br /> GA Bălți
+				Cluj-Napoca / Costești
+			</p>
+		</div>
+		<div class="table-wrap">
+			<table>
+				<thead>
+					<tr>
+						<th colspan=3>Tur</th>
+						<th rowspan=2><br />Distanta, km</th>
+						<th rowspan=2><br />Puncte de stationare</th>
+						<th colspan=3>Retur</th>
+					</tr>
+					<tr>
+						<th>Sosirea, ora/min</th>
+						<th>Stationare, min</th>
+						<th>Plecarea, ora/min</th>
+						<th>Sosirea, ora/min</th>
+						<th>Stationare, min</th>
+						<th>Plecarea, ora/min</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td>18:30</td>
+						<td>0</td>
+						<td>SA Gările şi Stațiile Auto fil. Bălți, str.Ștefan cel
+							Mare, 2</td>
+						<td>04:05</td>
+						<td></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>19:10</td>
+						<td>1</td>
+						<td>19:11</td>
+						<td>35</td>
+						<td>Rîșcani</td>
+						<td>03:24</td>
+						<td>1</td>
+						<td>03:25</td>
+					</tr>
+					<tr>
+						<td>19:21</td>
+						<td>9</td>
+						<td>19:30</td>
+						<td>40</td>
+						<td>SA Gările şi Stațiile Auto fil. Rîșcani</td>
+						<td>03:05</td>
+						<td>9</td>
+						<td>03:14</td>
+					</tr>
+					<tr>
+						<td>20:10</td>
+						<td>5</td>
+						<td>20:15</td>
+						<td>75</td>
+						<td>Costești</td>
+						<td>02:20</td>
+						<td>5</td>
+						<td>02:25</td>
+					</tr>
+					<tr>
+						<td>20:20</td>
+						<td>20</td>
+						<td>20:40</td>
+						<td>77</td>
+						<td>Vama Costești MD</td>
+						<td>01:55</td>
+						<td>20</td>
+						<td>02:15</td>
+					</tr>
+					<tr>
+						<td>20:41</td>
+						<td>20</td>
+						<td>21:01</td>
+						<td>77</td>
+						<td>Vama Costești Stânca RO</td>
+						<td>01:34</td>
+						<td>20</td>
+						<td>01:54</td>
+					</tr>
+					<tr>
+						<td>22:11</td>
+						<td>5</td>
+						<td>22:16</td>
+						<td>133</td>
+						<td>Botoșani, str. Garii,nr.1</td>
+						<td>00:19</td>
+						<td>5</td>
+						<td>00:24</td>
+					</tr>
+					<tr>
+						<td>22:56</td>
+						<td>10</td>
+						<td>23:06</td>
+						<td>175</td>
+						<td>Suceava, str.Ana Ipotescu</td>
+						<td>23:29</td>
+						<td>10</td>
+						<td>23:39</td>
+					</tr>
+					<tr>
+						<td>00:16</td>
+						<td>4</td>
+						<td>00:20</td>
+						<td>242</td>
+						<td>Campulung Moldovenesc, str Lidi</td>
+						<td>22:15</td>
+						<td>4</td>
+						<td>22:19</td>
+					</tr>
+					<tr>
+						<td>01:05</td>
+						<td>5</td>
+						<td>01:10</td>
+						<td>282</td>
+						<td>Vatra Dornei, str Vasile Deac 2</td>
+						<td>21:25</td>
+						<td>5</td>
+						<td>21:30</td>
+					</tr>
+					<tr>
+						<td>02:40</td>
+						<td>5</td>
+						<td>02:45</td>
+						<td>366</td>
+						<td>Bistrița, str. 1 Decembrie</td>
+						<td>19:50</td>
+						<td>5</td>
+						<td>19:55</td>
+					</tr>
+					<tr>
+						<td>04:05</td>
+						<td>5</td>
+						<td>04:10</td>
+						<td>440</td>
+						<td>Gherla</td>
+						<td>18:25</td>
+						<td>5</td>
+						<td>18:30</td>
+					</tr>
+					<tr>
+						<td>04:50</td>
+						<td>5</td>
+						<td>04:55</td>
+						<td>474</td>
+						<td>Aeroportul Internațional Avram Iancu Cluj</td>
+						<td>17:40</td>
+						<td>5</td>
+						<td>17:45</td>
+					</tr>
+					<tr>
+						<td>05:35</td>
+						<td></td>
+						<td></td>
+						<td>480</td>
+						<td>Cluj-Napoca</td>
+						<td></td>
+						<td></td>
+						<td>17:00</td>
+					</tr>
+				</tbody>
+			</table>
+			<p>Ruta internaţională Bălti-Cluj circulă zilnic în paritate cu
+				transportatorul român. Mijloacele de transport dispun numărul de
+				locuri de la 19. Scopul nostru este să transportăm cu siguranţă şi
+				comoditate pasagerii. Dacă aveţi careva întrebări nu ezitaţi să ne
+				telefonaţi sau să ne scrieţi pe e-mail. Suntem mereu la dispoziţia
+				dumneavoastră.</p>
+		</div>
+	</div>
+
+	<!-- footer section -->
+	<div id="footer">
+		<div class="container">
+			<div class="bottom-col">
+				<h4>Urmăriți-ne</h4>
+				<ul class="social">
+					<li class="facebook"><a href="#"></a></li>
+					<li class="twitter"><a href="#"></a></li>
+					<li class="odnoklassniki"><a href="#"></a></li>
+				</ul>
+
+			</div>
+			<div class="bottom-col">
+				<h4>Detalii de contact</h4>
+				<div class="address">
+					<img style="padding-top: 1px" src="images/address.png" alt=""><strong>Adresa:
+					</strong> Bălți, str. Independentii 70/6
+				</div>
+				<div class="address">
+					<img style="padding-top: 1px" src="images/phone.png" alt=""><strong>Telefon:
+					</strong>+37369265880
+				</div>
+				<div class="address">
+					<img style="padding-top: 3px" src="images/mail.png" alt=""><strong>Email:
+					</strong> arinturtrans@mail.ru
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
